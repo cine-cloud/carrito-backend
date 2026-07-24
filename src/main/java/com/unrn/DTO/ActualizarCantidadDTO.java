ba059c1a7c036493a40efd1f642db7f5c60efe95
Karen Ruiz <katiiruiz797@gmail.com>
2026-07-24 19:06:23 -0300

    feat: guardar cambios en carrito antes de merge

--- src/main/java/com/unrn/DTO/ActualizarCantidadDTO.java
+++
@@ -1,9 +0,0 @@
-package com.unrn.DTO;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-
-public record ActualizarCantidadDTO(
-        @NotNull Integer peliculaId,
-        @Positive int cantidad
-) {}
--- src/main/java/com/unrn/DTO/EliminarItemDTO.java
+++
@@ -1,7 +0,0 @@
-package com.unrn.DTO;
-
-import jakarta.validation.constraints.NotNull;
-
-public record EliminarItemDTO(
-        @NotNull Integer peliculaId
-) {}
